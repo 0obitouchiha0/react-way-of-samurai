ce7bbe9db37c33db63ee5374b2109473f1b7639a
danil136 <77704781+danil136@users.noreply.github.com>
2021-11-23 20:16:08 +0300

    added css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/App.css';
+import './App.css';
 import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
 import Profile from './components/Profile.jsx'
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,27 @@
+import classes from './Profile.module.css';
+
 const Profile = () => {
   return (
-    <main className="main">
-        <div className="main__header">
+    <main className={classes.main}>
+        <div className={classes.main__header}>
           <img src="https://c4.wallpaperflare.com/wallpaper/855/699/771/pexels-photo-101472-1024x683-pexels-photo-101472-1024x683-wallpaper-preview.jpg" alt="" />
         </div>
-        <div className="main__desc">
-          <div className="main__avatar">
+        <div className={classes.main__desc}>
+          <div className={classes.main__avatar}>
             <img src="https://www.meme-arsenal.com/memes/825b9b3ef148fbf2b20f47951e170699.jpg" alt="" />
           </div>
         </div>
-        <div className="main__posts">
-          <div className="main__post__title">
+        <div className={classes.main__posts}>
+          <div className={classes.main__posts__title}>
             My posts
           </div>
-          <div className="post">
+          <div className={classes.post}>
             New post
           </div>
-          <div className="post">
+          <div className={classes.post}>
             post1
           </div>
-          <div className="post">
+          <div className={classes.post}>
             post2
           </div>
         </div>
